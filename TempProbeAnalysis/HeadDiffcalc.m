f6c4316955eb586687c61ebcf158500883da1e51
bld456 <lange679@umn.edu>
2017-03-06 00:24:26 -0600

    working 1dtemppro!

--- TempProbeAnalysis/HeadDiffcalc.m
+++ TempProbeAnalysis/HeadDiffcalc.m
@@ -5,48 +5,52 @@ clear all
 close all
 format long g
 %import necessary files
-PZCC = load( 'C:\SecondCreekGit\Scripts\PZ-CC_161005_Calib20161001_1300_results.mat');
+PZCC1 = load( 'C:\SecondCreekGit\Scripts\PZ-CC_161005_Calib20161001_1300_results.mat');
+PZCC2 = load( 'C:\SecondCreekGit\Scripts\PZ-CC_161005_Calib20161001_1300_results.mat');
+
 SG11 = load('C:\SecondCreekGit\Scripts\SG-1_1st_Position_161005_Calib20160531_0945_results.mat');
 SG12 = load('C:\SecondCreekGit\Scripts\SG-1_2nd_Position_161005_Calib20161001_1430_results.mat');
 
 %extract date data
 dateSG11 = datenum(SG11.DivDateTime);
 dateSG12 = datenum(SG12.DivDateTime);
-datePZCC = datenum(PZCC.DivDateTime);
+datePZCC1 = datenum(PZCC1.DivDateTime);
+datePZCC2 = datenum(PZCC2.DivDateTime);
+
 
 %find starting and ending times
-Time1 = max([min(dateSG11), min(datePZCC)]);
-TimeEnd = min( [max(dateSG11), max(datePZCC)]);
+Time1 = max([min(dateSG11), min(datePZCC1)]);
+TimeEnd = min( [max(dateSG11), max(datePZCC1)]);
 
 %truncate each time array
-[PZCCstrt,col] = find(datePZCC == Time1);
+[PZCC1strt,col] = find(datePZCC1 == Time1);
 [SG11strt,col] = find(dateSG11 == Time1);
 
 
-[PZCCend,col] = find(datePZCC == TimeEnd);
+[PZCC1end,col] = find(datePZCC1 == TimeEnd);
 [SG11end,col] = find(dateSG11 == TimeEnd);
 
 
-PZCCadjustedT = datePZCC(PZCCstrt : PZCCend);
+PZCC1adjustedT = datePZCC1(PZCC1strt : PZCC1end);
 SG11adjustedT= dateSG11(SG11strt : SG11end);
 
 %change date format
 SG11adjustedT = datestr(SG11adjustedT, 'mm/dd/yyyy HH:MM');
-PZCCadjustedT = datestr(PZCCadjustedT, 'mm/dd/yyyy HH:MM');
+PZCC1adjustedT = datestr(PZCC1adjustedT, 'mm/dd/yyyy HH:MM');
 
-PZCCadjustedT= table(PZCCadjustedT);
+PZCC1adjustedT= table(PZCC1adjustedT);
 SG11adjustedT= table(SG11adjustedT);
 
 
 %import head data
 headSG11 = SG11.DivTotHead/100;
-headPZCC = PZCC.DivTotHead/100;
+headPZCC1 = PZCC1.DivTotHead/100;
 
 %truncate head data
-PZCCadjustedH= headPZCC(PZCCstrt : PZCCend);
+PZCC1adjustedH= headPZCC1(PZCC1strt : PZCC1end);
 SG11adjustedH= headSG11(SG11strt : SG11end);
 %calculate difference
-headDiff1 = PZCCadjustedH-SG11adjustedH;
+headDiff1 = PZCC1adjustedH-SG11adjustedH;
 
 
 %output file
@@ -67,38 +71,38 @@ end
 %repeat for second chunk of summer
 
 %find starting and ending times
-Time1 = max([min(dateSG12), min(datePZCC)]);
-TimeEnd = min( [max(dateSG12), max(datePZCC)]);
+Time1 = max([min(dateSG12), min(datePZCC2)]);
+TimeEnd = min( [max(dateSG12), max(datePZCC2)]);
 
 %truncate each time array
-[PZCCstrt,col] = find(datePZCC == Time1);
+[PZCC2strt,col] = find(datePZCC2 == Time1);
 [SG12strt,col] = find(dateSG12 == Time1);
 
 
-[PZCCend,col] = find(datePZCC == TimeEnd);
+[PZCC2end,col] = find(datePZCC2 == TimeEnd);
 [SG12end,col] = find(dateSG12 == TimeEnd);
 
 
-PZCCadjustedT = datePZCC(PZCCstrt : PZCCend);
-SG12adjustedT= dateSG11(SG12strt : SG11end);
+PZCC2adjustedT = datePZCC2(PZCC2strt : PZCC2end);
+SG12adjustedT= dateSG12(SG12strt : SG12end);
 
 %change date format
 SG12adjustedT = datestr(SG12adjustedT, 'mm/dd/yyyy HH:MM');
-PZCCadjustedT = datestr(PZCCadjustedT, 'mm/dd/yyyy HH:MM');
+PZCC2adjustedT = datestr(PZCC2adjustedT, 'mm/dd/yyyy HH:MM');
 
-PZCCadjustedT= table(PZCCadjustedT);
+PZCC2adjustedT= table(PZCC2adjustedT);
 SG12adjustedT= table(SG12adjustedT);
 
 
 %import head data
 headSG12 = SG12.DivTotHead/100;
-headPZCC = PZCC.DivTotHead/100;
+headPZCC2 = PZCC2.DivTotHead/100;
 
 %truncate head data
-PZCCadjustedH= headPZCC(PZCCstrt : PZCCend);
-SG12adjustedH= headSG11(SG12strt : SG12end);
+PZCC2adjustedH= headPZCC2(PZCC2strt : PZCC2end);
+SG12adjustedH= headSG12(SG12strt : SG12end);
 %calculate difference
-headDiff2 = PZCCadjustedH-SG12adjustedH;
+headDiff2 = PZCC2adjustedH-SG12adjustedH;
 
 
 
