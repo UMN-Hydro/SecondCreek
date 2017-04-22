18b32ab950954c6294cc444833a1a5e1eb029df0
bld456 <lange679@umn.edu>
2017-04-22 13:15:31 -0500

    new script to visualize model error

--- TempProbeAnalysis/HeadDiffcalc.m
+++ TempProbeAnalysis/HeadDiffcalc.m
@@ -5,8 +5,8 @@ clear all
 close all
 format long g
 %import necessary files
-PZCC1 = load( 'C:\SecondCreekGit\Scripts\PZ-Out_161005_Calib20161001_1545_results.mat');
-PZCC2 = load( 'C:\SecondCreekGit\Scripts\PZ-Out_161005_Calib20161001_1545_results.mat');
+PZCC1 = load( 'C:\SecondCreekGit\Scripts\PZ-CW_161005_Calib20161001_1400_results.mat');
+PZCC2 = load( 'C:\SecondCreekGit\Scripts\PZ-CW_161005_Calib20161001_1400_results.mat');
 
 SG11 = load('C:\SecondCreekGit\Scripts\SG-1_1st_Position_161005_Calib20160531_0945_results.mat');
 SG12 = load('C:\SecondCreekGit\Scripts\SG-1 2nd Position_161005_Calib20161001_1430_resultsOLD.mat');
@@ -59,7 +59,7 @@ headDiff1 = PZCC1adjustedH-SG11adjustedH;
  %fileID = 'TPA_headDiff1_1DTempPro.csv';
 
  
-fileID = fopen('C:\SecondCreekGit\TempProbeAnalysis\HeadDiff1_PZOSG.csv','w');
+fileID = fopen('C:\SecondCreekGit\TempProbeAnalysis\HeadDiff1_PZCWSG.csv','w');
  [nrows, ncols] = size(SG11adjustedH) ;
 
 
@@ -115,7 +115,7 @@ headDiff2 = PZCC2adjustedH-SG12adjustedH;
 
 
 
- fileID = fopen('C:\SecondCreekGit\TempProbeAnalysis\HeadDiff2_PZOSG.csv','w');
+ fileID = fopen('C:\SecondCreekGit\TempProbeAnalysis\HeadDiff2_PZCWSG.csv','w');
 
 [nSG11, ncols] =  size(SG11adjustedH);
 [nSG12, col] = size(SG12adjustedH);
