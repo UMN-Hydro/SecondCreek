76bc129bf1e1cd91daad6f7b9e6d02591e124eca
bld456 <lange679@umn.edu>
2017-10-25 14:13:58 -0500

    updated head scaling script to reflect new organization of git rep

--- Scripts/Kcalc.py
+++ Scripts/Kcalc.py
@@ -5,16 +5,16 @@ from matplotlib import pyplot as plt
 
 PZCCstickup = 154.94 #distance from PZCC TOC to streambed, cm
 PZCClength = 182.88 #length of PZCC casing, cm
-dsCC = PZCClength - PZCCstickup
+dsCC = PZCClength - PZCCstickup #distance from streambed to screened interval
 
-PZCWstickup = 140.97 #distance from PZCW TOC to streambed, cm
-PZCWlength = 182.88 #length of PZCW casing, cm
-dsCW = PZCWlength - PZCWstickup
-
-
-
-PZCIstickup = 0.0 #distance from PZCC TOC to streambed, cm
-PZCIlength = 0.0 #length of PZCC casing, cm
+#PZCWstickup = 140.97 #distance from PZCW TOC to streambed, cm
+#PZCWlength = 182.88 #length of PZCW casing, cm
+#dsCW = PZCWlength - PZCWstickup #distance from streambed to screened interval
+#
+#
+#
+#PZCIstickup = 0.0 #distance from PZCC TOC to streambed, cm
+#PZCIlength = 0.0 #length of PZCC casing, cm
 
 #Script to take average q output from 1DTempProbePro and a deltaH time series to calculate average K. Additionally
 #it scales the delta H data to 0.3 meters, (the distance between the top and bottom temperature probes) for future use with 1DTempPro.
@@ -23,14 +23,14 @@ PZCIlength = 0.0 #length of PZCC casing, cm
 #Variables names ending in 2 indicate that they are the end of summer, aug 1 - october
     
 
-q1= -0.040889 #modeled average flux from 1dtempro
-q2= -.045143 #modeled average flux from 1dtempro
+q= -0.040889 #modeled average flux from 1dtempro
 
 
-#read in head difference timeseries
 
-dh1 =pd.read_csv('HeadDiff1_PZCCSG.csv', sep= ',', header = None ) #first half of summer
-dh2= pd.read_csv('HeadDiff2_PZCCSG.csv', sep= ',', header = None ) #second half of summer
+#read in head difference timeseries. These time series were calculated from the PZdata dependent on the measured depths fro OCTOBER
+
+dh1 =pd.read_csv('C:\\SecondCreekGit\\SCRIPT OUTPUTS\\HEAD DIFFERENCES\\HeadDiff1_PZCCSG.csv', sep= ',', header = None ) #first half of summer
+dh2= pd.read_csv('C:\\SecondCreekGit\\SCRIPT OUTPUTS\\HEAD DIFFERENCES\\HeadDiff2_PZCCSG.csv', sep= ',', header = None ) #second half of summer
 
 #rename colums for ease of use
 
@@ -59,32 +59,29 @@ plt.show()
 # then convert k/ds to k that will be used by 1d temp pro
 
 #Darcy's law to calculade k/ds
-k1overds= -dh1['deltah'].mean() / q1
-k2overds= -dh2['deltah'].mean() / q2
-
-ds1 = 429.548 - 429.092 #distance from sg1 to PZCC (vertical distance between measurment devices, surveyed)
-ds2 = 429.548 - 428.818 #distance from sg2 to PZCC
+k1overds= -dh1['deltah'].mean() / q
+k2overds= -dh2['deltah'].mean() / q
 
 #calculate hydraulic conductivity
-k1= k1overds *ds1
-k2= k2overds *ds2
+k1= k1overds *dsCC
+k2= k2overds *dsCC
 
 print 'k1 = %f' % (k1)
 print 'k2 = %f' %(k2)
 
 #scale dh to .3 meters for 1dtemppro. Also make it negative to indicate higher head in the stream bed
-dh1['deltah'] = dh1['deltah'] * -.3/ds1
-dh2['deltah'] = dh2['deltah'] * -.3/ds2 
+dh1['deltah'] = dh1['deltah'] * -.3/dsCC
+dh2['deltah'] = dh2['deltah'] * -.3/dsCC 
 
 #Save head data as a csv suitable for 1dtempProbePro. This requires reloading the
 #csv as a numpy array so that the delimiter can be ', '. Pandas doesn't support multi
 #character delimiters.
-dh1.to_csv('scaleddh1.csv',sep =',',date_format='%m/%d/%Y %H:%M', header = False)
-dh2.to_csv('scaleddh2.csv',sep =',',date_format='%m/%d/%Y %H:%M', header = False)
-dh1temp = np.loadtxt('scaleddh1.csv', dtype =str , delimiter = ',')
-dh2temp = np.loadtxt('scaleddh2.csv', dtype =str, delimiter = ',')
-np.savetxt('scaleddh1CC.csv',dh1temp,fmt= '%s', delimiter = ', ')
-np.savetxt('scaleddh2CC.csv',dh2temp,fmt = '%s', delimiter = ', ')
+dh1.to_csv('C:\\SecondCreekGit\\SCRIPT OUTPUTS\\HEAD DIFFERENCES\\Scaled using PZStickup\\scaleddh1.csv',sep =',',date_format='%m/%d/%Y %H:%M', header = False)
+dh2.to_csv('C:\\SecondCreekGit\\SCRIPT OUTPUTS\\HEAD DIFFERENCES\\Scaled using PZStickup\\scaleddh2.csv',sep =',',date_format='%m/%d/%Y %H:%M', header = False)
+dh1temp = np.loadtxt('C:\\SecondCreekGit\\SCRIPT OUTPUTS\\HEAD DIFFERENCES\\Scaled using PZStickup\\scaleddh1.csv', dtype =str , delimiter = ',')
+dh2temp = np.loadtxt('C:\\SecondCreekGit\\SCRIPT OUTPUTS\\HEAD DIFFERENCES\\Scaled using PZStickup\\scaleddh2.csv', dtype =str, delimiter = ',')
+np.savetxt('C:\\SecondCreekGit\\SCRIPT OUTPUTS\\HEAD DIFFERENCES\\Scaled using PZStickup\\scaleddh1CC.csv',dh1temp,fmt= '%s', delimiter = ', ')
+np.savetxt('C:\\SecondCreekGit\\SCRIPT OUTPUTS\\HEAD DIFFERENCES\\Scaled using PZStickup\\scaleddh2CC.csv',dh2temp,fmt = '%s', delimiter = ', ')
 #
 
 
