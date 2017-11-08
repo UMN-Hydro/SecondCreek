c353f374fffb7490bca37297b44de0522e306e99
bld456 <lange679@umn.edu>
2017-11-08 10:59:23 -0600

    plotted q TS for several situations

--- Scripts/plot_q.py
+++ Scripts/plot_q.py
@@ -13,22 +13,22 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 #inputs
-fil ='C:\\SecondCreekGit\\SCRIPT OUTPUTS\\HEAD DIFFERENCES\\Scaled using PZStickup\\scaleddh1CW_shifted.csv' #path to dh series used in 1dtemp
-K  = 0.02144#modeled from 1dtemp
+fil ='C:\\SecondCreekGit\\SCRIPT OUTPUTS\\HEAD DIFFERENCES\\Scaled using PZStickup\\scaleddh2CW.csv' #path to dh series used in 1dtemp
+K  =0.035916#modeled from 1dtemp
 q_avg = -0.0037182 #avg q from 1dtemp
 
 
 #read head timeseries
 dh =pd.read_csv(fil, sep= ',', header = None ) #first half of summer, meters
 dh.rename(columns={0: 'date', 1: 'deltah'}, inplace = True)
-dh['date']= pd.to_datetime(dh['date'], format= ' %m/%d/%Y %H:%M')
+dh['date']= pd.to_datetime(dh['date'], format= '%m/%d/%Y %H:%M')
 
 dh['deltah'] = dh['deltah'] /.3
 dh['q'] = dh['deltah'] * -K
 
-title ='q at TPA modeled from shifted PZCW data' #plot title
+title ='q at TPA- K from  whole summer- modeled from PZCW data' #plot title
 
-
+#plt.plot(dh['date'], dh['q'])
 dh.q.plot()
 plt.axhline(-q_avg)
 #plt.locator_params(numticks = 4)
@@ -44,3 +44,4 @@ plt.axhline(-q_avg)
 plt.xlabel('Date')
 plt.ylabel('q, m/d, positive is upwards ')
 plt.title(title)
+plt.savefig("C:\\SecondCreekGit\\SCRIPT OUTPUTS\\q Time series plots\\"+title)
