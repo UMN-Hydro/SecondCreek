62e7056a1529e2259747993f3f7d56ca4c7919da
bld456 <lange679@umn.edu>
2017-11-21 21:00:15 -0600

    monkeying with atmospheric pressure file

--- Scripts/SCDiver_calc8_ay.m
+++ Scripts/SCDiver_calc8_ay.m
@@ -41,7 +41,7 @@ clear all, close all, fclose all;
 
 % - Choose one transducer
 % DivLabel = 'SG-1 2015';
-% DivLabel = 'PZ-In';
+ DivLabel = 'PZ-In';
  %DivLabel = 'PZ-Out';
 % DivLabel = 'PZ-Bank';
  %DivLabel = 'PZ-CW'; %added summer 2016
@@ -53,7 +53,7 @@ clear all, close all, fclose all;
 % DivLabel = 'C1';
 % DivLabel = 'D1';
 % DivLabel = 'E1';
-DivLabel = 'SG-1_1st_Position';
+%DivLabel = 'SG-1_1st_Position';
 %  DivLabel = 'SG-1_2nd_Position';
 
 % =========================
@@ -636,7 +636,7 @@ if strcmp(date_label, '161005')
 
     elseif strcmp(DivLabel, 'PZ-In') 
         PVC_Elev = 429.098 * 100; % m -> cm [total station transit 10/01/16]
-        SCDiver_fil = 'C:\SecondCreekGit\2016_data\2016_10_02\CSV_edit\pzi_6_161005160841_M9505.CSV';
+        SCDiver_fil = 'C:\SecondCreekGit\DATA\2016_head_data\2016_10_02\CSV_edit\pzi_6_161005160841_M9505.CSV';
        ManualDate = '2016/10/01'; ManualTime = '13:00:00'; PVC_DepthToWater = 22.7; % cm
 %         ManualDate = '2016/05/31'; ManualTime = '09:00:00'; PVC_DepthToWater = 56.37; % cm
 
@@ -754,7 +754,7 @@ if strcmp(date_label, '161005')
 
     % *********************
 
-    SCBaro_fil = 'C:\SecondCreekGit\2016_data\2016_10_02\CSV_edit\a2_baro_161005161148_M9484.csv';
+    SCBaro_fil = 'C:\SecondCreekGit\DATA\2016_head_data\2016_10_02\CSV_edit\a2_baro_161005161148_M9484.csv';
     Baro_Int_Min = 15; % time interval in min
         
 end
