d0e6ca9a364a703ec49f4f344beb0024a88f5878
bld456 <lange679@umn.edu>
2018-04-02 14:30:02 -0500

    poster draft 2

--- Scripts/SensAnalysis_plot.R
+++ Scripts/SensAnalysis_plot.R
@@ -14,35 +14,43 @@ require(zoo)
 require(TSAUMN)
 require(lubridate)
 require(tidyselect)
-#to vary dot size, add a erro column to each input, do size = eroor
 
 Dispersivity <- read.csv(file = "C:\\SecondCreekGit\\Presentations and figures\\GSA NC poster\\Data\\Dispersivity.csv")
 Dispersivity$X = NULL
-#Dispersivity$error = Dispersivity$error *1000
-Disp_plot =ggplot(data = Dispersivity, aes (x= dispersivity, y= value, colour = variable, group = variable, size = error))+geom_point()+labs(title = "Dispersivity sensitivity analysis") + ylab("Hydraulic conductivity, m/d") + geom_line(size =1) +scale_colour_discrete(name="Location") + xlab("Dispersivity, m")
-Disp_plot = Disp_plot +theme(plot.title = element_text(size=28, face = "bold"),axis.text=element_text(size=14),  axis.title=element_text(size=24))
-Disp_plot = Disp_plot+ scale_radius(range=c(1, 20)) #issues because of the zero value
+Disp_plot =ggplot(data = Dispersivity, aes (x= dispersivity, y= value, colour = variable, group = variable, size = error))+geom_point()+ ylab("Hydraulic conductivity, m/d") + geom_line(size =1) +scale_colour_discrete(name="Location") + xlab("Dispersivity, m")
+Disp_plot = Disp_plot +theme(axis.text=element_text(size=20),  axis.title=element_text(size=45))
+Disp_plot = Disp_plot+ scale_radius(range=c(1, 15)) 
+Disp_plot = Disp_plot+ ylim(low = 0, high = .18)
+Disp_plot= Disp_plot+ theme(legend.position="none")
 
+ggsave('C:\\SecondCreekGit\\Presentations and figures\\GSA NC poster\\Disp_plot.png', units = 'in', width = 10, height = 10)
 
 
 
 ThermalConductivity <- read.csv( file = "C:\\SecondCreekGit\\Presentations and figures\\GSA NC poster\\Data\\ThermalConductivity.csv")
 ThermalConductivity$X = NULL
-#ThermalConductivity$error
 
+TC_plot = ggplot(data = ThermalConductivity, aes (x= thermal.conductivity, y= value, size = error,colour = variable, group = variable))+geom_point()+ geom_line(size = 2) +scale_colour_discrete(name="Location") +ylab('')+ xlab("Sediment thermal conductivity, W/m*C")
+TC_plot = TC_plot +theme(axis.text=element_text(size=20),  axis.title=element_text(size=30))
+TC_plot =TC_plot+scale_radius(range=c(1, 15))
+TC_plot =TC_plot+ylim(low = 0, high = .18)
+TC_plot= TC_plot+ theme(legend.position="none")
 
-TC_plot = ggplot(data = ThermalConductivity, aes (x= thermal.conductivity, y= value, size = error,colour = variable, group = variable))+geom_point()+labs(title = "Sediment thermal conductivity sensitivity analysis") + ylab("Hydraulic conductivity, m/d") + geom_line(size = 2) +scale_colour_discrete(name="Location") + xlab("Thermal conductivity, W/m*C")
-TC_plot = TC_plot +theme(plot.title = element_text(size=28, face = "bold"),axis.text=element_text(size=14),  axis.title=element_text(size=24))
-TC_plot =TC_plot+scale_radius(range=c(1, 20))
+ggsave('C:\\SecondCreekGit\\Presentations and figures\\GSA NC poster\\TC_plot.png', units = 'in', width = 10, height = 10)
+
 
 
 
 HeatCapacity <- read.csv(file = "C:\\SecondCreekGit\\Presentations and figures\\GSA NC poster\\Data\\HeatCapacity.csv")
 HeatCapacity$X=NULL 
-HC_plot = ggplot(data = HeatCapacity, aes (x= Saturated.heat.capacity, y= value, colour = variable, group = variable, size = error))+geom_point()+labs(title = "Saturated medium heat capacity sensitivity analysis") + ylab("Saturated medium heat capacity, J/(m^3 *C)") + geom_line(size = 2)
+HC_plot = ggplot(data = HeatCapacity, aes (x= Saturated.heat.capacity, y= value, colour = variable, group = variable, size = error))+geom_point()+ xlab("Saturated medium heat capacity, J/(m^3 *C)") +geom_line(size = 2)+ylab('')
 
-HC_plot = HC_plot +theme(plot.title = element_text(size=28, face = "bold"),axis.text=element_text(size=14),  axis.title=element_text(size=24))
-HC_plot= HC_plot+ scale_radius(range=c(1, 20))
+HC_plot = HC_plot +theme(axis.text=element_text(size=20),  axis.title=element_text(size=30))
+HC_plot= HC_plot+ scale_radius(range=c(1, 15))
+HC_plot= HC_plot+ ylim(low = 0, high = .18)
+HC_plot= HC_plot+ theme(legend.position="none")
+ggsave('C:\\SecondCreekGit\\Presentations and figures\\GSA NC poster\\HC_plot.png', units = 'in', width = 10, height = 10)
+
 
 print(Disp_plot)
 print (TC_plot)
--- Scripts/conjoined_plots.R
+++ Scripts/conjoined_plots.R
@@ -11,22 +11,29 @@ x_min =  as.POSIXct("2016-06-04 00:00:00", format = "%Y-%m-%d %H:%M:%S")
 x_max = as.POSIXct("2016-10-01 01:30:00", format = "%Y-%m-%d %H:%M:%S")
 
 
-plot1 <-ggplot(data = test, aes(x= Date, y = value, color = variable, na.rm = FALSE)) + geom_line(aes(y= stream_center, col = "stream_center"), size = 2)+geom_line(aes(y= stream_west, col = "stream_west"), size = 2)+geom_line(aes(y= west_wetland, col = "west_wetland"), size =2 )+theme_gray()+ theme(legend.position="none")
-plot2 <-ggplot(data = test, aes(x= Date, y=value, na.rm = FALSE))+geom_line(aes(y=PRCP, col ="PRCP"), size = 2, color='black') + ylab('Precip, in.') +theme_gray()
+plot1 <-ggplot(data = test, aes(x= Date, y = value, color = variable, na.rm = FALSE)) +xlab('')+ geom_line(aes(y= stream_center, col = "stream_center"), size = 2)+geom_line(aes(y= stream_west, col = "stream_west"), size = 2)+geom_line(aes(y= west_wetland, col = "west_wetland"), size =2 )+theme_gray()+ theme(legend.position="none")
+plot2 <-ggplot(data = test, aes(x= Date, y=value, na.rm = FALSE))+geom_line(aes(y=PRCP, col ="PRCP"), size = 2, color='black') + ylab('Precip, in.') +theme_gray()+xlab('')
 
 plot1 <- plot1 + scale_x_datetime(limits=c(x_min, x_max))
 plot2 <- plot2 + scale_x_datetime(limits=c(x_min, x_max))
 
-plot1 = plot1 +ylab("Flux, m/d. Positive is upwards flux.") + labs(title = 'Daily hyporheic flux, 2016')
-
-plot2 = plot2  + labs(title = 'Precipitation at Embarass, MN weather station')
+plot1 = plot1 +ylab("Hyporheic flux, m/d.") 
 
 
 
-plot1 <- plot1 +theme(plot.title = element_text(size=28, face = "bold"),axis.text=element_text(size=14),  axis.title=element_text(size=24))
-plot2 <- plot2 + theme(plot.title = element_text(size=28, face = "bold"),axis.text=element_text(size=14), axis.title=element_text(size=24))
+
+
+plot1 <- plot1 +theme(axis.text=element_text(size=20),  axis.title=element_text(size=28))
+plot2 <- plot2 + theme(axis.text=element_text(size=20), axis.title=element_text(size=28))
+
+jpeg('C:\\SecondCreekGit\\Presentations and figures\\GSA NC poster\\Combo_plot.jpg', width = 2000, height = 1700,res = 150 )
+
+
 
 grid.newpage()
 pushViewport(viewport(layout = grid.layout(2, 1)))
+#jpeg('C:\\SecondCreekGit\\Presentations and figures\\GSA NC poster\\Combo_plot.jpg', units = 'in', width = 12, height = 12 )
+
 print(plot2, vp = viewport(layout.pos.row = 2, layout.pos.col=1))
 print(plot1, vp = viewport(layout.pos.row = 1, layout.pos.col=1))
+dev.off()
